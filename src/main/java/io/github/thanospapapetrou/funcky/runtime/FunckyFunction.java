8aa43f5dc14cba0f3975b43e5b6ec7ca5c2201d8
Athanasios Papapetrou <athanasios.papapetrou@netcompany.com>
2025-11-01 12:21:17 +0200

    Funcky functions as BiFunctions

--- src/main/java/io/github/thanospapapetrou/funcky/runtime/FunckyFunction.java
+++ src/main/java/io/github/thanospapapetrou/funcky/runtime/FunckyFunction.java
@@ -1,16 +1,20 @@
 package io.github.thanospapapetrou.funcky.runtime;
 
+import java.util.function.BiFunction;
+
 import javax.script.ScriptContext;
 
 import io.github.thanospapapetrou.funcky.compiler.ast.FunckyExpression;
 
-public non-sealed abstract class FunckyFunction extends FunckyValue implements Comparable<FunckyFunction> {
+public non-sealed abstract class FunckyFunction extends FunckyValue
+        implements BiFunction<FunckyExpression, ScriptContext, FunckyValue>, Comparable<FunckyFunction> {
     protected final FunckyFunctionType type;
 
     protected FunckyFunction(final FunckyFunctionType type) {
         this.type = type;
     }
 
+    @Override
     public abstract FunckyValue apply(final FunckyExpression argument, final ScriptContext context);
 
     @Override
