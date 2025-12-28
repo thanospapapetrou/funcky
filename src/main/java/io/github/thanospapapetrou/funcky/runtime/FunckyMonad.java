db1ebcff9cde198448c5e69399b42e8626f29752
Athanasios Papapetrou <athanasios.papapetrou@netcompany.com>
2025-12-29 01:04:24 +0200

    Monad printing

--- src/main/java/io/github/thanospapapetrou/funcky/runtime/FunckyMonad.java
+++ src/main/java/io/github/thanospapapetrou/funcky/runtime/FunckyMonad.java
@@ -8,7 +8,7 @@ import io.github.thanospapapetrou.funcky.compiler.ast.FunckyLiteral;
 import io.github.thanospapapetrou.funcky.runtime.types.FunckyMonadicType;
 
 public final class FunckyMonad extends FunckyValue {
-    private static final String FORMAT = "Monad %1$s";
+    private static final String FORMAT = "monad_%1$x";
 
     private final FunckyMonadicType type;
     private final Supplier<FunckyExpression> base;
@@ -36,20 +36,22 @@ public final class FunckyMonad extends FunckyValue {
     @Override
     public int compareTo(final FunckyValue value) {
         if (value instanceof FunckyMonad monad) {
-            return ((base == null) ? ((monad.base == null) ? 0 : -1) : ((monad.base == null) ? 1
-                    : base.get().eval(engine.getContext()).compareTo(monad.base.get().eval(engine.getContext()))));
+            final int comparison = type.compareTo(monad.type);
+            return (comparison == 0)
+                    ? base.get().eval(engine.getContext()).compareTo(monad.base.get().eval(engine.getContext()))
+                    : comparison;
         }
         return super.compareTo(value);
     }
 
     @Override
     public int hashCode() {
-        return ((base == null) ? 0 : base.get().eval(engine.getContext()).hashCode());
+        return type.hashCode() + base.get().eval(engine.getContext()).hashCode();
     }
 
     @Override
     public String toString() {
         base.get().eval(engine.getContext());
-        return String.format(FORMAT, getType());
+        return String.format(FORMAT, hashCode());
     }
 }
